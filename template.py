647426a012839e3ae75b065d2749bb85a620bab8
Jason Francis <jarfranc@indiana.edu>
2019-09-23 14:53:06 -0400

    Updates for arcade projects

--- template.py
+++ template.py
@@ -1,4 +1,4 @@
-import sys, logging, json
+import sys, logging, json, open_color, arcade
 
 #check to make sure we are running the right version of Python
 version = (3,7)
@@ -8,22 +8,75 @@ assert sys.version_info >= version, "This script requires at least Python {0}.{1
 logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = ""
 
+
+class Window(arcade.Window):
+
+    def __init__(self, width, height, title):
+
+        # Call the parent class's init function
+        super().__init__(width, height, title)
+
+        # Make the mouse disappear when it is over the window.
+        # So we just see our object, not the pointer.
+        self.set_mouse_visible(False)
+
+        arcade.set_background_color(open_color.black)
+
+
+
+    def setup(self):
+        pass 
+
+    def update(self, delta_time):
+        pass
+
+    def on_draw(self):
+        """ Called whenever we need to draw the window. """
+        arcade.start_render()
+
+
+
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """ Called to update our objects. Happens approximately 60 times per second."""
+        pass
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
+
+    def on_key_press(self, key, modifiers):
+        """ Called whenever the user presses a key. """
+        if key == arcade.key.LEFT:
+            print("Left")
+        elif key == arcade.key.RIGHT:
+            print("Right")
+        elif key == arcade.key.UP:
+            print("Up")
+        elif key == arcade.key.DOWN:
+            print("Down")
+
+    def on_key_release(self, key, modifiers):
+        """ Called whenever a user releases a key. """
+        pass
 
 
 def main():
-    game = {}
-    with open('zork.json') as json_file:
-        game = json.load(json_file)
-    # Your game goes here!
-
-    current = 'WHOUS'
+    window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    arcade.run()
 
 
-    return True
-
-
-
-#if we are running this from the command line, run main
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
